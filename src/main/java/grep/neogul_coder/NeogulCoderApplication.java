d00feb63b2e2f9269df299427b572006226ae7d9
mikm7243 <mikm7243@naver.com>
2025-06-26 17:44:00 +0900

    Inital commit

---
+++ src/main/java/grep/neogul_coder/NeogulCoderApplication.java
@@ -0,0 +1,13 @@
+package grep.neogul_coder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeogulCoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeogulCoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grep/neogul_coder/NeogulCoderApplicationTests.java
@@ -0,0 +1,13 @@
+package grep.neogul_coder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeogulCoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
