9cab3d767ee972cb592ac468c1e14bf48cc5b64f
Tokwasp <mikm7243@naver.com>
2025-07-08 17:33:59 +0900

    feature: 스터디 모집글, 리뷰 엔티티 작성

---
+++ src/main/java/grep/neogul_coder/domain/review/tag/ReviewMessage.java
@@ -0,0 +1,11 @@
+package grep.neogul_coder.domain.review.tag;
+
+public enum ReviewMessage {
+    ;
+
+    private final String description;
+
+    ReviewMessage(String description) {
+        this.description = description;
+    }
+}
