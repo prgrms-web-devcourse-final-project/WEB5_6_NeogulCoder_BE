1883c73390b014f6d5aa9f639353a6ce0e1610cd
pia01190 <piakim0119@gmail.com>
2025-07-09 16:27:56 +0900

    [EA3-77] refactor: category enum description 추가

--- src/main/java/grep/neogul_coder/domain/study/enums/Category.java
+++ src/main/java/grep/neogul_coder/domain/study/enums/Category.java
@@ -1,6 +1,22 @@
 package grep.neogul_coder.domain.study.enums;
 
 public enum Category {
-    LANGUAGE, IT, EXAM, FINANCE, MANAGEMENT, DESIGN,
-    ART, PHOTO_VIDEO, BEAUTY, SPORTS, HOBBY, ETC
+    LANGUAGE("어학"),
+    IT("IT"),
+    EXAM("고시/자격증"),
+    FINANCE("금융"),
+    MANAGEMENT("경영"),
+    DESIGN("디자인"),
+    ART("예술"),
+    PHOTO_VIDEO("사진/영상"),
+    BEAUTY("뷰티"),
+    SPORTS("스포츠"),
+    HOBBY("취미"),
+    ETC("기타");
+
+    private final String description;
+
+    Category(String description) {
+        this.description = description;
+    }
 }
