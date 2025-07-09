33b5c38cbd1cc21f0747ec748259a5a5d0637d12
dbrkdgus00 <chldlfla@gmail.com>
2025-07-09 15:41:05 +0900

    [EA3-73] rename : 패키지 구조 변경

--- src/main/java/grep/neogul_coder/domain/groupchat/controller/dto/GroupChatSwaggerResponse.java
+++ src/main/java/grep/neogul_coder/domain/groupchat/controller/dto/GroupChatSwaggerResponse.java
@@ -1,4 +1,4 @@
-package grep.neogul_coder.domain.groupchat.dto;
+package grep.neogul_coder.domain.groupchat.controller.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
