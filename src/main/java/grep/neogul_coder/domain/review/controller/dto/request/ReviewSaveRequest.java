f33ace20e7db870f1e11ba5827096e8b1523c5cb
Tokwasp <mikm7243@naver.com>
2025-07-09 23:20:15 +0900

    [EA3-81] feature: 리뷰 태그 조회 및 리뷰 생성 스웨거 작성

---
+++ src/main/java/grep/neogul_coder/domain/review/controller/dto/request/ReviewSaveRequest.java
@@ -0,0 +1,17 @@
+package grep.neogul_coder.domain.review.controller.dto.request;
+
+import grep.neogul_coder.domain.review.ReviewType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class ReviewSaveRequest {
+
+    @Schema(example = "EXCELLENT", description = "리뷰 타입")
+    private ReviewType reviewType;
+
+    @Schema(example = "[항상 먼저 도와주고 분위기를 이끌어주는 팀원이었어요., 책임감이 넘치고 맡은 일 이상으로 기여해줘서 감동이었어요.]", description = "리뷰 메시지들")
+    private List<String> reviewTag;
+}
