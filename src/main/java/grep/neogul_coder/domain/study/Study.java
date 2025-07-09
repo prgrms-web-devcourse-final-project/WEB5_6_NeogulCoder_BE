e0b983ab9614a470fea50789f472e3df2e6119b6
pia01190 <piakim0119@gmail.com>
2025-07-09 11:55:11 +0900

    [EA3-77] feature: 스터디, 스터디 멤버 엔티티 작성

---
+++ src/main/java/grep/neogul_coder/domain/study/Study.java
@@ -0,0 +1,52 @@
+package grep.neogul_coder.domain.study;
+
+import grep.neogul_coder.domain.study.enums.Category;
+import grep.neogul_coder.domain.study.enums.StudyType;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "study")
+public class Study {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studyId;
+
+    private Long originStudyId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Category category;
+
+    @Column(nullable = false)
+    private int capacity;
+
+    @Column(nullable = false)
+    private int currentCount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StudyType studyType;
+
+    private String location;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+    private String introduction;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    private Boolean isActivated;
+
+    private Boolean isDeleted = false;
+}
