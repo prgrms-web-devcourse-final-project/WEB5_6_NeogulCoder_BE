882ff2631bf0c93371a7c3f2ebfaac46272204e9
dbrkdgus00 <chldlfla@gmail.com>
2025-07-10 16:41:04 +0900

    [EA3-73] feature : 그룹채팅 스웨거 builder로 리팩토링

--- src/main/java/grep/neogul_coder/domain/groupchat/controller/dto/GroupChatSwaggerRequest.java
+++ src/main/java/grep/neogul_coder/domain/groupchat/controller/dto/GroupChatSwaggerRequest.java
@@ -1,27 +1,25 @@
 package grep.neogul_coder.domain.groupchat.controller.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
 
+@Getter
 @Schema(description = "Swagger용 채팅 메시지 전송 요청 DTO")
 public class GroupChatSwaggerRequest {
 
-    @Schema(description = "보낸 사람 ID", example = "3")
+    @Schema(description = "보낸 사람 ID", example = "456")
     private Long senderId;
 
-    @Schema(description = "채팅방 ID", example = "101")
+    @Schema(description = "채팅방 ID", example = "100")
     private Long roomId;
 
     @Schema(description = "보낼 메시지", example = "안녕하세요!")
     private String message;
 
 
-
-    public Long getSenderId() { return senderId; }
     public void setSenderId(Long senderId) { this.senderId = senderId; }
 
-    public Long getRoomId() { return roomId; }
     public void setRoomId(Long roomId) { this.roomId = roomId; }
 
-    public String getMessage() { return message; }
     public void setMessage(String message) { this.message = message; }
 }
