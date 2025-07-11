4ce750b8ae2cf3f9bfd189aca500d50e02b3d3f3
endorsement0912 <ef032550@naver.com>
2025-07-11 23:47:26 +0900

    [EA3-78] chore: H2-PostgreSQL 호환을 위해 StudyPost의 ID 생성 전략을 AUTO로 변경

--- src/main/java/grep/neogul_coder/domain/studypost/StudyPost.java
+++ src/main/java/grep/neogul_coder/domain/studypost/StudyPost.java
@@ -18,7 +18,7 @@ import jakarta.validation.constraints.NotBlank;
 public class StudyPost extends BaseEntity {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
   @ManyToOne(fetch = FetchType.LAZY, optional = false)
