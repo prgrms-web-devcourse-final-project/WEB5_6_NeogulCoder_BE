31d45bd9f08bac5fc4f6be90cac5adf72a3660d5
endorsement0912 <ef032550@naver.com>
2025-07-12 15:58:48 +0900

    [EA3-79] chore: DB 호환을 위해 ID 생성 전략을 AUTO로 변경

--- src/main/java/grep/neogul_coder/domain/comment/Comment.java
+++ src/main/java/grep/neogul_coder/domain/comment/Comment.java
@@ -11,7 +11,7 @@ import jakarta.validation.constraints.NotBlank;
 public class Comment {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
   @Column(nullable = false)
