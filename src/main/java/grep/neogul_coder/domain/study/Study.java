fddded5110c84c0844c9bdc3858f3f8ad45595c9
Tokwasp <mikm7243@naver.com>
2025-07-14 12:13:58 +0900

    [EA3-118] refactor: 회원 식별자 이름 변경

--- src/main/java/grep/neogul_coder/domain/study/Study.java
+++ src/main/java/grep/neogul_coder/domain/study/Study.java
@@ -12,7 +12,7 @@ public class Study extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long studyId;
+    private Long id;
 
     private Long originStudyId;
 
