6359da52417c327655a50d633b8e9961e92d156e
pia01190 <piakim0119@gmail.com>
2025-07-14 18:07:04 +0900

    [EA3-122] refactor: 스터디 엔티티 null 제약 삭제

--- src/main/java/grep/neogul_coder/domain/study/Study.java
+++ src/main/java/grep/neogul_coder/domain/study/Study.java
@@ -19,34 +19,26 @@ public class Study extends BaseEntity {
 
     private Long originStudyId;
 
-    @Column(nullable = false)
     private String name;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
     private Category category;
 
-    @Column(nullable = false)
     private int capacity;
 
-    @Column(nullable = false)
     private int currentCount;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
     private StudyType studyType;
 
     private String location;
 
-    @Column(nullable = false)
     private LocalDate startDate;
 
-    @Column(nullable = false)
     private LocalDate endDate;
 
     private String introduction;
 
-    @Column(nullable = false)
     private String imageUrl;
 
     private boolean isFinished;
