5b2ae25e8528267cf7a071f36b77a2be46275815
Tokwasp <mikm7243@naver.com>
2025-07-15 15:47:45 +0900

    [EA3-132] feature: 리뷰 대상 회원들 조회시 식별자 값 추가

--- src/main/java/grep/neogul_coder/domain/review/controller/dto/response/ReviewTargetUsersInfo.java
+++ src/main/java/grep/neogul_coder/domain/review/controller/dto/response/ReviewTargetUsersInfo.java
@@ -25,20 +25,25 @@ public class ReviewTargetUsersInfo {
 
     public static ReviewTargetUsersInfo of(Study study, List<User> users) {
         List<TargetUserInfo> targetUserInfos = users.stream()
-                .map(user -> new TargetUserInfo(user.getNickname()))
+                .map(user -> new TargetUserInfo(user.getId(), user.getNickname()))
                 .toList();
 
-        StudyInfo studyInfo = new StudyInfo(study.getName(), study.getImageUrl());
+        StudyInfo studyInfo = new StudyInfo(study.getId(), study.getName(), study.getImageUrl());
         return new ReviewTargetUsersInfo(targetUserInfos, studyInfo);
     }
 
     @ToString
     @Getter
     static class TargetUserInfo {
+
+        @Schema(example = "3", description = "회원 ID")
+        private long userId;
+
         @Schema(example = "짱구", description = "리뷰 대상 닉네임")
         private String nickname;
 
-        private TargetUserInfo(String nickname) {
+        public TargetUserInfo(long userId, String nickname) {
+            this.userId = userId;
             this.nickname = nickname;
         }
     }
@@ -46,13 +51,18 @@ public class ReviewTargetUsersInfo {
     @ToString
     @Getter
     static class StudyInfo {
+
+        @Schema(example = "2", description = "스터디 ID")
+        private long studyId;
+
         @Schema(example = "자바 스터디", description = "스터디 이름")
         private String studyName;
 
         @Schema(example = "www.s3.com", description = "스터디 이미지 URL")
         private String imageUrl;
 
-        private StudyInfo(String studyName, String imageUrl) {
+        public StudyInfo(long studyId, String studyName, String imageUrl) {
+            this.studyId = studyId;
             this.studyName = studyName;
             this.imageUrl = imageUrl;
         }
