a6fd5fb9bf9297ed3bb7d63343d410bfd08bfcbd
pia01190 <piakim0119@gmail.com>
2025-07-15 16:24:33 +0900

    [EA3-133] refactor: 스터디 관리 컨트롤러 RequestMapping 경로 수정

--- src/main/java/grep/neogul_coder/domain/study/controller/StudyManagementController.java
+++ src/main/java/grep/neogul_coder/domain/study/controller/StudyManagementController.java
@@ -6,34 +6,34 @@ import grep.neogul_coder.global.response.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api/studies")
+@RequestMapping("/api/studies/{studyId}")
 @RestController
 public class StudyManagementController implements StudyManagementSpecification {
 
-    @DeleteMapping("/{studyId}/me")
+    @DeleteMapping("/me")
     public ApiResponse<Void> leaveStudy(@PathVariable("studyId") Long studyId) {
         return ApiResponse.noContent();
     }
 
-    @DeleteMapping("/{studyId}/users/{userId}")
+    @DeleteMapping("/users/{userId}")
     public ApiResponse<Void> deleteMember(@PathVariable("studyId") Long studyId,
                                           @PathVariable("userId") Long userId) {
         return ApiResponse.noContent();
     }
 
-    @PostMapping("/{studyId}/delegate")
+    @PostMapping("/delegate")
     public ApiResponse<Void> delegateLeader(@PathVariable("studyId") Long studyId,
                                             @RequestBody DelegateLeaderRequest request) {
         return ApiResponse.noContent();
     }
 
-    @PostMapping("/{studyId}/extend")
+    @PostMapping("/extend")
     public ApiResponse<Void> extendStudy(@PathVariable("studyId") Long studyId,
                                          @RequestBody @Valid ExtendStudyRequest request) {
         return ApiResponse.noContent();
     }
 
-    @PostMapping("/{studyId}/join")
+    @PostMapping("/join")
     public ApiResponse<Void> joinExtendStudy(@PathVariable("studyId") Long studyId) {
         return ApiResponse.noContent();
     }
