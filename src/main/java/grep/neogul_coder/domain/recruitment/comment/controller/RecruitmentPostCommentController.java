42c2d005cb422aaf7c7de2f745e0266a92dd9643
Tokwasp <mikm7243@naver.com>
2025-07-17 10:29:54 +0900

    feature: 모집글 댓글 API 명세 추가

--- src/main/java/grep/neogul_coder/domain/recruitment/comment/controller/RecruitmentPostCommentController.java
+++ src/main/java/grep/neogul_coder/domain/recruitment/comment/controller/RecruitmentPostCommentController.java
@@ -1,5 +1,33 @@
 package grep.neogul_coder.domain.recruitment.comment.controller;
 
-public class RecruitmentPostCommentController {
+import grep.neogul_coder.domain.recruitment.comment.controller.dto.request.RecruitmentCommentSaveRequest;
+import grep.neogul_coder.domain.recruitment.comment.controller.dto.request.RecruitmentCommentUpdateRequest;
+import grep.neogul_coder.global.auth.Principal;
+import grep.neogul_coder.global.response.ApiResponse;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/recruitment-posts/{post-id}/comments")
+@RestController
+public class RecruitmentPostCommentController implements RecruitmentPostCommentSpecification {
+
+    @PostMapping
+    public ApiResponse<Void> save(@PathVariable("post-id") long postId,
+                                  @RequestBody RecruitmentCommentSaveRequest request,
+                                  @AuthenticationPrincipal Principal userDetails) {
+        return ApiResponse.noContent();
+    }
+
+    @PutMapping("/{comment-id}")
+    public ApiResponse<Void> update(@PathVariable("comment-id") long commentId,
+                                    @RequestBody RecruitmentCommentUpdateRequest request,
+                                    @AuthenticationPrincipal Principal userDetails) {
+        return ApiResponse.noContent();
+    }
+
+    @DeleteMapping("/{comment-id}")
+    public ApiResponse<Void> delete(@PathVariable("comment-id") long commentId,
+                                    @AuthenticationPrincipal Principal userDetails) {
+        return ApiResponse.noContent();
+    }
 }
--- src/main/java/grep/neogul_coder/domain/recruitment/comment/controller/RecruitmentPostCommentSpecification.java
+++ src/main/java/grep/neogul_coder/domain/recruitment/comment/controller/RecruitmentPostCommentSpecification.java
@@ -1,5 +1,7 @@
 package grep.neogul_coder.domain.recruitment.comment.controller;
 
+import grep.neogul_coder.domain.recruitment.comment.controller.dto.request.RecruitmentCommentSaveRequest;
+import grep.neogul_coder.domain.recruitment.comment.controller.dto.request.RecruitmentCommentUpdateRequest;
 import grep.neogul_coder.global.auth.Principal;
 import grep.neogul_coder.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,5 +11,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 public interface RecruitmentPostCommentSpecification {
 
     @Operation(summary = "모집글 댓글 작성", description = "모집글에 대한 댓글을 작성 합니다.")
-    ApiResponse<Void> save(Principal userDetails);
+    ApiResponse<Void> save(long postId, RecruitmentCommentSaveRequest request, Principal userDetails);
+
+    @Operation(summary = "모집글 댓글 수정", description = "모집글에 대한 댓글을 수정 합니다.")
+    ApiResponse<Void> update(long commentId, RecruitmentCommentUpdateRequest request, Principal userDetails);
+
+    @Operation(summary = "모집글 댓글 삭제", description = "모집글에 대한 댓글을 삭제 합니다.")
+    ApiResponse<Void> delete(long commentId, Principal userDetails);
 }
