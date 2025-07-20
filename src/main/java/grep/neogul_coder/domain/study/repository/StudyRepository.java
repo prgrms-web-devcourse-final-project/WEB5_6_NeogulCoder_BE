81cb6da271f89d2b8636d486044951537b905320
pia01190 <piakim0119@gmail.com>
2025-07-20 18:47:19 +0900

    [EA3-133] refactor: 스터디 삭제 쿼리 오류 해결

--- src/main/java/grep/neogul_coder/domain/study/repository/StudyRepository.java
+++ src/main/java/grep/neogul_coder/domain/study/repository/StudyRepository.java
@@ -1,10 +1,11 @@
 package grep.neogul_coder.domain.study.repository;
 
 import grep.neogul_coder.domain.study.Study;
-import grep.neogul_coder.domain.study.enums.Category;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 import java.util.List;
 
@@ -14,4 +15,8 @@ public interface StudyRepository extends JpaRepository<Study, Long> {
     Optional<Study> findByIdAndActivatedTrue(Long studyId);
 
     Optional<Study> findByOriginStudyIdAndActivatedTrue(Long originStudyId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Study s set s.activated = false where s.id = :studyId")
+    void deactivateByStudyId(@Param("studyId") Long studyId);
 }
