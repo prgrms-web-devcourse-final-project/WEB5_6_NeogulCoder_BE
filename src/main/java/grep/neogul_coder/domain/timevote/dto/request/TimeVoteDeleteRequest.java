cca6dcb089a5e19213ad74ae2e4b94808de02e18
endorsement0912 <ef032550@naver.com>
2025-07-22 12:33:50 +0900

    [EA3-134] refactor: 스터디 모임시간 조율 관련 요청 DTO 수정

--- src/main/java/grep/neogul_coder/domain/timevote/dto/request/TimeVoteDeleteRequest.java
+++ src/main/java/grep/neogul_coder/domain/timevote/dto/request/TimeVoteDeleteRequest.java
@@ -1,22 +1,20 @@
 package grep.neogul_coder.domain.timevote.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.time.LocalDateTime;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Schema(description = "스터디 모임 일정 조율 - 단일 시간 삭제 요청 DTO")
+@Schema(description = "스터디 모임 일정 조율 - 전체 지우기 요청 DTO")
 public class TimeVoteDeleteRequest {
-
-  @Schema(description = "투표 기간 ID", example = "5")
-  private Long periodId;
 
   @Schema(description = "스터디 멤버 ID", example = "12")
   private Long studyMemberId;
 
-  @Schema(description = "삭제할 시작 시간", example = "2025-07-16T10:00:00")
-  private LocalDateTime startTime;
+  private TimeVoteDeleteRequest() {}
 
-  @Schema(description = "삭제할 종료 시간", example = "2025-07-16T13:00:00")
-  private LocalDateTime endTime;
+  @Builder
+  private TimeVoteDeleteRequest(Long studyMemberId) {
+    this.studyMemberId = studyMemberId;
+  }
 }
--- src/main/java/grep/neogul_coder/domain/timevote/dto/request/TimeVoteUpdateRequest.java
+++ src/main/java/grep/neogul_coder/domain/timevote/dto/request/TimeVoteUpdateRequest.java
@@ -1,24 +1,26 @@
 package grep.neogul_coder.domain.timevote.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Schema(description = "스터디 모임 일정 조율 - 단일 가능 시간 수정 요청 DTO")
+@Schema(description = "스터디 모임 일정 조율 - 가능 시간 수정 요청 DTO")
 public class TimeVoteUpdateRequest {
 
-  @NotNull
-  @Schema(description = "투표 ID", example = "100")
-  private Long voteId;
+  @Schema(description = "스터디 멤버 ID", example = "12")
+  private Long studyMemberId;
 
-  @NotNull
-  @Schema(description = "변경할 시작 시간", example = "2025-07-16T15:00:00")
-  private LocalDateTime startTime;
+  @Schema(description = "시간대 리스트", example = "[2025-07-15T10:00:00, 2025-07-15T11:00:00]")
+  private List<LocalDateTime> timeSlots;
 
-  @NotNull
-  @Schema(description = "변경할 종료 시간", example = "2025-07-16T17:00:00")
-  private LocalDateTime endTime;
+  private TimeVoteUpdateRequest() {}
 
+  @Builder
+  private TimeVoteUpdateRequest(Long studyMemberId, List<LocalDateTime> timeSlots) {
+    this.studyMemberId = studyMemberId;
+    this.timeSlots = timeSlots;
+  }
 }
