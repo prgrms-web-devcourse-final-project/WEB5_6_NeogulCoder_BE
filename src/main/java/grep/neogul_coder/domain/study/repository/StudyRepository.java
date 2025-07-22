23b9161349833d88c652d402706061e504125e7b
pia01190 <piakim0119@gmail.com>
2025-07-22 17:08:51 +0900

    [EA3-167] feature: 종료 7일 전 스터디 찾는 기능

--- src/main/java/grep/neogul_coder/domain/study/repository/StudyRepository.java
+++ src/main/java/grep/neogul_coder/domain/study/repository/StudyRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.List;
 
@@ -19,4 +20,7 @@ public interface StudyRepository extends JpaRepository<Study, Long> {
     @Modifying(clearAutomatically = true)
     @Query("update Study s set s.activated = false where s.id = :studyId")
     void deactivateByStudyId(@Param("studyId") Long studyId);
+
+    @Query("select s from Study s where s.endDate >= :endDateStart and s.endDate < :endDateEnd and s.finished = false and s.activated = true")
+    List<Study> findStudiesEndingIn7Days(@Param("endDateStart") LocalDateTime endDateStart, @Param("endDateEnd") LocalDateTime endDateEnd);
 }
