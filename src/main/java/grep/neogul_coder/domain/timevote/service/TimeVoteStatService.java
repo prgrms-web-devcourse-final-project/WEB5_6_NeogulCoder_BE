ecf929db645be0a2b01f3a9b41be81b5cf4f4885
endorsement0912 <ef032550@naver.com>
2025-07-23 11:08:54 +0900

    [EA3-134] feature : 투표 통계관련 서비스 생성

---
+++ src/main/java/grep/neogul_coder/domain/timevote/service/TimeVoteStatService.java
@@ -0,0 +1,10 @@
+package grep.neogul_coder.domain.timevote.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TimeVoteStatService {
+
+}
