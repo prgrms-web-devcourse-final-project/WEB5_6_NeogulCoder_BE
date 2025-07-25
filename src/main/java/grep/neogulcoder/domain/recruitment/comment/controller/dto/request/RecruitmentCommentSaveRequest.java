ea8390b83600c093cec374932d361ffe16b8a9ab
Tokwasp <mikm7243@naver.com>
2025-07-25 21:42:40 +0900

    [EA3-138] refactor: 모집글 댓글 작성 URL 변경
    - 자원을 URI 표시 하는 것이 더 restful 하므로 변경

--- src/main/java/grep/neogulcoder/domain/recruitment/comment/controller/dto/request/RecruitmentCommentSaveRequest.java
+++ src/main/java/grep/neogulcoder/domain/recruitment/comment/controller/dto/request/RecruitmentCommentSaveRequest.java
@@ -8,16 +8,13 @@ import lombok.Getter;
 @Getter
 public class RecruitmentCommentSaveRequest {
 
-    @Schema(example = "2", description = "모집글 ID")
-    private long postId;
-
     @Schema(example = "저도 참여 할래요!", description = "모집글 내용")
     private String content;
 
     private RecruitmentCommentSaveRequest() {
     }
 
-    public RecruitmentPostComment toEntity(RecruitmentPost post, long userId){
+    public RecruitmentPostComment toEntity(RecruitmentPost post, long userId) {
         return RecruitmentPostComment.builder()
                 .recruitmentPost(post)
                 .userId(userId)
