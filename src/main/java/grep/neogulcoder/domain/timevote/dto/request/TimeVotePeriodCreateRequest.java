b4940371af195c3e744f7e2bf5911016dfacacd1
endorsement0912 <ef032550@naver.com>
2025-07-28 15:48:31 +0900

    [EA3-183] refactor : 엔티티들에서 toEntities, toEntity를 TimeVoteMapper로 분리

--- src/main/java/grep/neogulcoder/domain/timevote/dto/request/TimeVotePeriodCreateRequest.java
+++ src/main/java/grep/neogulcoder/domain/timevote/dto/request/TimeVotePeriodCreateRequest.java
@@ -1,6 +1,5 @@
 package grep.neogulcoder.domain.timevote.dto.request;
 
-import grep.neogulcoder.domain.timevote.TimeVotePeriod;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
@@ -11,11 +10,11 @@ import lombok.Getter;
 @Schema(description = "스터디 모임 일정 조율 - 팀장이 가능 시간 요청을 생성할 때 사용하는 요청 DTO")
 public class TimeVotePeriodCreateRequest {
 
-  @NotNull
+  @NotNull(message = "시작일은 필수입니다.")
   @Schema(description = "시작일", example = "2025-07-25T00:00:00")
   private LocalDateTime startDate;
 
-  @NotNull
+  @NotNull(message = "종료일은 필수입니다.")
   @Schema(description = "종료일", example = "2025-07-30T23:59:59")
   private LocalDateTime endDate;
 
@@ -25,17 +24,5 @@ public class TimeVotePeriodCreateRequest {
   private TimeVotePeriodCreateRequest(LocalDateTime startDate, LocalDateTime endDate) {
     this.startDate = startDate;
     this.endDate = endDate;
-  }
-
-  public TimeVotePeriod toEntity(Long studyId, LocalDateTime adjustedEndDate) {
-    return TimeVotePeriod.builder()
-        .startDate(this.startDate)
-        .endDate(adjustedEndDate)
-        .studyId(studyId)
-        .build();
-  }
-
-  public static TimeVotePeriodCreateRequest of(LocalDateTime startDate, LocalDateTime endDate) {
-    return new TimeVotePeriodCreateRequest(startDate, endDate);
   }
 }
