41b6f6c6d66380324ac876a96db1e1d3d0487fba
endorsement0912 <ef032550@naver.com>
2025-07-29 16:06:09 +0900

    [EA3-183] chore : 불필요한 import문 정리

--- src/main/java/grep/neogulcoder/domain/timevote/TimeVoteStat.java
+++ src/main/java/grep/neogulcoder/domain/timevote/TimeVoteStat.java
@@ -9,7 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Version;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
