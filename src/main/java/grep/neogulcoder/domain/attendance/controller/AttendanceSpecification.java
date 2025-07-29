835f3c1852f8136a8f90141cbebb5253b8b87be3
pia01190 <piakim0119@gmail.com>
2025-07-29 14:12:02 +0900

    [EA3-205] refactor: 응답값 ResponseEntity로 변경

--- src/main/java/grep/neogulcoder/domain/attendance/controller/AttendanceSpecification.java
+++ src/main/java/grep/neogulcoder/domain/attendance/controller/AttendanceSpecification.java
@@ -5,13 +5,14 @@ import grep.neogulcoder.global.auth.Principal;
 import grep.neogulcoder.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 
 @Tag(name = "Attendance", description = "출석 API")
 public interface AttendanceSpecification {
 
     @Operation(summary = "출석 조회", description = "일주일 단위로 출석을 조회합니다.")
-    ApiResponse<AttendanceInfoResponse> getAttendances(Long studyId, Principal userDetails);
+    ResponseEntity<ApiResponse<AttendanceInfoResponse>> getAttendances(Long studyId, Principal userDetails);
 
     @Operation(summary = "출석 체크", description = "스터디에 출석을 합니다.")
-    ApiResponse<Long> createAttendance(Long studyId, Principal userDetails);
+    ResponseEntity<ApiResponse<Long>> createAttendance(Long studyId, Principal userDetails);
 }
