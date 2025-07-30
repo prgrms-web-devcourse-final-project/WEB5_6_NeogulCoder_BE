062e41812a46091013b454368692eb0c5e6541d6
hyeunS-P <hseric0816@gmail.com>
2025-07-30 17:03:57 +0900

    fix: 회원 탈퇴 시 비밀번호 확인을 체크하지 않던 문제 해결

--- src/main/java/grep/neogulcoder/domain/users/controller/dto/request/PasswordRequest.java
+++ src/main/java/grep/neogulcoder/domain/users/controller/dto/request/PasswordRequest.java
@@ -9,4 +9,7 @@ public class PasswordRequest {
     @NotBlank(message = "비밀번호를 입력하세요")
     private String password;
 
+    @NotBlank(message = "비밀번호 확인을 입력하세요")
+    private String passwordCheck;
+
 }
