64a7efed5a0364e3461263dd37160e0751bae2a4
pia01190 <piakim0119@gmail.com>
2025-09-05 21:22:24 +0900

    [EA3-211] refactor: 스터디 인원수 증가, 감소 로직 리팩터링

--- src/main/java/grep/neogulcoder/domain/study/service/StudyManagementServiceFacade.java
+++ src/main/java/grep/neogulcoder/domain/study/service/StudyManagementServiceFacade.java
@@ -1,29 +1,63 @@
 package grep.neogulcoder.domain.study.service;
 
-import grep.neogulcoder.domain.study.Study;
-import grep.neogulcoder.domain.study.repository.StudyRepository;
 import grep.neogulcoder.global.exception.business.BusinessException;
-import grep.neogulcoder.global.exception.business.NotFoundException;
 import jakarta.persistence.OptimisticLockException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import static grep.neogulcoder.domain.study.exception.code.StudyErrorCode.*;
+import static grep.neogulcoder.domain.study.exception.code.StudyErrorCode.STUDY_MEMBER_COUNT_UPDATE_FAILED;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class StudyManagementServiceFacade {
 
     private final StudyMemberCountService studyMemberCountService;
+    private static final int MAX_RETRY = 3;
 
     public void increaseMemberCount(Long studyId, Long userId) {
-        studyMemberCountService.increaseMemberCount(studyId, userId);
+        int retry = 0;
+        while (retry < MAX_RETRY) {
+            try {
+                studyMemberCountService.increaseMemberCount(studyId);
+                return;
+            } catch (OptimisticLockException | ObjectOptimisticLockingFailureException e) {
+                retry++;
+
+                try {
+                    Thread.sleep(30);
+                } catch (InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        }
+
+        log.warn("스터디 currentCount 증가 실패 (studyId={}, userId={})", studyId, userId);
+        throw new BusinessException(STUDY_MEMBER_COUNT_UPDATE_FAILED);
     }
 
     public void decreaseMemberCount(Long studyId, Long userId) {
-        studyMemberCountService.decreaseMemberCount(studyId, userId);
+        int retry = 0;
+        while (retry < MAX_RETRY) {
+            try {
+                studyMemberCountService.decreaseMemberCount(studyId);
+                return;
+            } catch (OptimisticLockException | ObjectOptimisticLockingFailureException e) {
+                retry++;
+
+                try {
+                    Thread.sleep(30);
+                } catch (InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        }
+
+        log.warn("스터디 currentCount 감소 실패 (studyId={}, userId={})", studyId, userId);
+        throw new BusinessException(STUDY_MEMBER_COUNT_UPDATE_FAILED);
     }
 }
